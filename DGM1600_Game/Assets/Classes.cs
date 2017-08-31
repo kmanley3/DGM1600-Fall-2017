f4e8ffeb65ce811cb4e85c84b5ec46555af6822f
Unknown <kirstenmanley7@gmail.com>
2017-08-30 20:07:10 -0600

    Classes.cs added

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000;
+		public float fuel = 0.01f;
+		
+		private string name = "Bob";
+	}
+	public class Two {
+		private int time = 6;
+		public float responsibilities = 4000.48f;
+		public string zinger = "Everything is fine.";
+	}
+	public class Three {
+		public float problems = 99.9f;
+		public int femaleDog = 0;
+		private string imCheeky = "I got 99 problems but a bitch ain't one.";
+	}
+}
