2da877d58740f70e0e2d9890dd6f7c54b87e39c5
Unknown <kirstenmanley7@gmail.com>
2017-09-11 20:14:13 -0600

    Updated Conditionals.cs

--- DGM1600_Game/Assets/Conditionals.cs
+++ DGM1600_Game/Assets/Conditionals.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Conditionals : MonoBehaviour {
+//CONVENTIONS AND SYNTAX:
+//Syntax is essentially Scripting Grammar. It is imperative to put components in the correct order. Also you can't misspell any words within the syntax. It must be right 100% of the time.
+public int health = "Duck";
+public string greeting = 40f;
+// These variables have bad syntax. The integer shown above has the syntax of a string, whereas the string above has the syntax of a Float.
+public int health = 40;
+public string greeting = "Hello!";
+public float strength = 60f;
+//These variables have correct syntax.
+//Conventions are not mandatory. It's more of a courtesy to other programmers that you may be working with. When using Braces, some programmers prefer to have the opening brace on the same line as the statement.
+//(Or whatever else you may be using)
+
 
 public string stopLight = "Red";
 
