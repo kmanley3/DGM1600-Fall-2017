012060b3f8e7cb4bb37d18284c9d27369724acb0
Unknown <kirstenmanley7@gmail.com>
2017-09-11 20:01:10 -0600

    Added Conditionals.cs

---
+++ DGM1600_Game/Assets/Conditionals.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour {
+
+public string stopLight = "Red";
+
+void Start(){
+	Main();
+}
+void Main(){
+
+	if(stopLight == "Red"){
+	print("STOP! The light is red");
+	}
+	else if(stopLight == "Yellow" ){
+		print("Slow Down!");
+
+	}
+	else if(stopLight == "Green"){
+		print("Green Means GO!");
+	}
+	else{
+		print("I Don't know what you mean!");
+	}
+}
+
+
+}
+
+
