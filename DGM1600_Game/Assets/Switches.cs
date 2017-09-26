a60d1035b55dce30845cad1d9828c6dad23bec0f
Unknown <kirstenmanley7@gmail.com>
2017-09-25 19:57:24 -0600

    Switches.cs added

---
+++ DGM1600_Game/Assets/Switches.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour {
+	public string townCenter;
+	public string theDaedalus;
+
+
+	// Use this for initialization
+	void Start () {
+		switch(townCenter){
+			case "main":
+				print("Welcome to Main Street!");
+			break;
+			case "blacksmith":
+				print("The Blacksmith grumbles as you pick through the sword bin.");
+			break;
+			case "bakery":
+				print("Mmmm... Baked goodness!");
+			break;
+			case "morgue":
+				print("Welcome to the house of the dead :D");
+			break;
+			default:
+				print("I don't know what you're talking about!");
+			break;
+		}
+		
+		switch(theDaedalus){
+			case "chandra":
+				print("Can I help you?");
+			break;
+			case "xander":
+				print("I'm a little busy right now, but what do you need?");
+			break;
+			default:
+				print("That person is not on the ship.");
+			break;
+		} 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- DGM1600_Game/Assets/While.cs
+++ DGM1600_Game/Assets/While.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour {
+public class While : MonoBehaviour {
 
-string message = "Help Me!";
+
+	public int bottles = 0;
 
 	// Use this for initialization
 	void Start () {
-		print("Hello World!");
+		while(bottles < 100){
+			bottles ++;
+			print(bottles + " bottles of beer on the wall!");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print(message);
+		
 	}
 }
