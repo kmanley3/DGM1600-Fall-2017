fac954bbe4a4b80c32d391e531c9a8cb9e38fb54
Unknown <kirstenmanley7@gmail.com>
2017-09-27 20:17:11 -0600

    added LogicalOP.cs

---
+++ DGM1600_Game/Assets/LogicalOP.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalOP : MonoBehaviour {
+	public string luke = "Jedi";
+	public string vader = "Sith";
+	public bool leia = true;
+	public string darkSide = "Darth Maul";
+	public string lightSide = "Obi Wan";
+	public int num1 = 10;
+	public float num2 = 10.5f;
+
+	// Use this for initialization
+	void Start () {
+		if(luke == "Jedi" && vader == "Nanny"){	
+			print("Vader's yo nanny");
+		}
+		else if(luke == "Jawa" && vader == "Sith"){
+			print("Utinni!");
+		}
+		else if(luke == "Jedi" && vader == "Sith"){
+			print("Nooo! That's not true.. That's impossible!");
+		}
+		else{
+			print("The End!");
+		}
+		if(leia){
+			print("No, there is another...");
+		}
+		else if(!leia){
+			print("Hurray! Now I can date Luke -- but Han is better.");
+		}
+		else
+			print("Hot shot first!");
+		if(lightSide == "Obi Wan" || darkSide == "Jar Jar Binks"){
+			print("Meesa Evil Sith Lord!");
+		}
+		else if(lightSide == "Boba Fett" || darkSide == "Darth Maul"){
+			print("As you wish, my master.");
+		}
+		else{
+			print("Not the younglings.");
+		}
+		if(num1 == 10 && num2 == 10.5){
+			print("Yes!");
+		}
+		else if(num1 == 10 && num2 == 10){
+			print("Almost!");
+		}
+		else{
+			print("No!");
+		}
+	}
+	
+	
+	// Update is called once per frame
+	//void Update () {}
+		
+	}
