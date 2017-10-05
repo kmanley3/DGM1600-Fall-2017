d9e978424050d35bc268b9e9bc7fcd2900a5ec3a
Unknown <kirstenmanley7@gmail.com>
2017-10-04 20:10:22 -0600

    added void functions.cs
    oooOOOoo

---
+++ DGM1600_Game/Assets/Void_Functions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Void_Functions : MonoBehaviour {
+
+
+public int photonTorpedoes;
+
+	// Use this for initialization
+	void Start () {
+		DeathStar(photonTorpedoes);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void DeathStar (int vent){
+		print("That's no moon... That's a space station!");
+
+		if(vent == 1){
+			print("Red Leader misses!");
+
+		}
+		else if(vent == 2){
+			print("Use the Force, Luke...");
+		}
+		else{
+			print("The Death Star blows up!");
+		}
+	}
+}
