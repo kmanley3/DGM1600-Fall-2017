a14c1f301cc4a2cabe3a6fd43983040e9047a609
Unknown <kirstenmanley7@gmail.com>
2017-10-11 20:23:28 -0600

    add ForEachList.cs

---
+++ DGM1600_Game/Assets/First Order.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FirstOrder : IComparable<FirstOrder> {
+
+		public string name;
+		public int power;
+		public int age;
+
+		public FirstOrder(string newName, int newPower, int newAge){
+			name = newName;
+			power = newPower;
+			age = newAge;
+		}
+
+		public int CompareTo(FirstOrder other)
+		{
+			if(other == null){
+				return 1;
+			}
+
+			return power - other.power;
+			
+		}
+
+}
---
+++ DGM1600_Game/Assets/ForEachList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForEachList : MonoBehaviour {
+	
+	
+	// Use this for initialization
+	void Start () {
+		List<FirstOrder> badguys = new List<FirstOrder>();
+
+		badguys.Add( new FirstOrder("Snoke",1000));
+		badguys.Add( new FirstOrder("General Hux",20));
+		badguys.Add( new FirstOrder("Kylo Ren",400));
+		badguys.Add( new FirstOrder("Captain Phasma",150));
+
+		badguys.Sort();
+
+		foreach(FirstOrder guy in badguys){
+			print(guy.name + " " + guy.power);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
