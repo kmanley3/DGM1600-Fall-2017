d7a8b000848ad7f13ad7e39a6842d7d4b21f35ce
kmanley3 <31229153+kmanley3@users.noreply.github.com>
2017-10-11 22:21:43 -0600

    ForEachLists.cs updated

--- DGM1600_Game/Assets/ForEachList.cs
+++ DGM1600_Game/Assets/ForEachList.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,6 +18,25 @@ public class ForEachList : MonoBehaviour {
 
 		foreach(FirstOrder guy in badguys){
 			print(guy.name + " " + guy.power);
+
+            List<FirstOrder> mostWanted = new List<FirstOrder>();
+
+		mostWanted.Add( new FirstOrder("Rey",24));
+		mostWanted.Add( new FirstOrder("FN-2187",25));
+		mostWanted.Add( new FirstOrder("Han Solo",66));
+		mostWanted.Add( new FirstOrder("Luke Skywalker",60));
+        mostWanted.Add( new FirstOrder("Leia Skywalker Solo",60));
+        mostWanted.Add( new FirstOrder("Poe Dameron",27));
+        mostWanted.Add( new FirstOrder("Chewbacca",79));
+        mostWanted.Add( new FirstOrder("BB-8",1));
+        mostWanted.Add( new FirstOrder("C-3PO", 100));
+        mostWanted.Add( new FirstOrder("R2-D2",102));
+
+		mostWanted.Sort();
+
+		foreach(FirstOrder guy in mostWanted){
+			print(guy.name + " " + guy.age);
+		}
 		}
 		
 	}
